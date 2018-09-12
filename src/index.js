bf874683245ecc2b0709bb17ebc4d403361503da
lexBogos <lex.bogos@gmail.com>
2018-09-12 15:01:03 +0300

    v1

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+var number_of_triangles = 0;
+for (var i = 0; i<preferences.length; i++)
+    
+	if  (i+1 == preferences[preferences[preferences[i]-1]-1] && preferences[i] != preferences[preferences[i]-1])  {
+		// console.log([preferences[i],preferences[preferences[i]-1],preferences[preferences[preferences[i]-1]-1]]);
+        number_of_triangles++;
+        preferences[i] = 0;
+        preferences[preferences[i]-1]=0;
+        preferences[preferences[preferences[i]-1]-1]=0;
+
+
+	}
+return number_of_triangles;		
+
 };
